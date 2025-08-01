d21f1fa6c9419bbf72d6ece6d1ceea5694bc039e
Javier Quiroga <contact@javierquiroga.com>
2025-08-01 22:03:57 +0100

    feat: Set up project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/base-url',
+  base: '/cribbage-score',
   plugins: [react()],
+  server: {
+    host: true, // Enables access from other devices on your network
+    port: 5173, // Optional: pick a custom port if needed
+  },
 });
